b848ddd1571ed58d3755a355fc8bfd202d980733
Jblack262 <JBlack262@west-mec.org>
2021-08-26 23:13:50 -0700

    new post form added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
-// import Post from './components/Post';
-
+import Post from './components/Post';
 class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      posts: [{
+        id: 1,
+        name: "John Doe",
+        text: "This is an example post."
+      }]
+    };
+  }
+
+  getNewPost(author, body) {
+    this.addComment(author, body);
+  }
+
+  addComment(name, text) {
+    const post = {
+      id: this.state.posts.length + 1,
+      name,
+      text
+    };
+    console.log(post)
+    this.setState({ post: this.state.posts.push(post)});
+  }
+
+  getPosts() {    
+    return this.state.posts.map((posts) => { 
+      return (
+        <Post 
+          name={posts.name} 
+          text={posts.text} 
+          key={posts.id}
+        />
+      ); 
+    });
+  }
+
   render() {
+    const posts = this.getPosts();
     return (
       <div className="App">
-        <Form />
-        {/* <Post name="bob" text="Hello world"/> */}
+        <Form 
+          newPost={this.getNewPost.bind(this)}
+        />
+        <div>
+          {posts}
+        </div>
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,69 +1,29 @@
 import React from 'react';
-import Post from './Post';
 
 class Form extends React.Component {
-  constructor() {
-    super();
-    
-    let form = this.state = {
-      posts: [{
-        id: 1,
-        name: "John Doe",
-        text: "This is an example post."
-      }]
-    };
-  }
-
   formSubmission = e => {
     e.preventDefault();
     let name = this._name;
     let text = this._text;
-    this.addComment(name.value, text.value);
-    console.log(this.state.posts)
-  };
-  
-  addComment(name, text) {
-    const post = {
-      id: this.state.posts.length + 1,
-      name,
-      text
-    };
-    console.log(post)
-    this.setState({ post: this.state.posts.push(post)}); // *new array references help React stay fast, so concat works better than push here.
-  }
 
-  getPosts() {    
-    return this.state.posts.map((posts) => { 
-      return (
-        <Post 
-          name={posts.name} 
-          text={posts.text} 
-          key={posts.id}
-        />
-      ); 
-    });
-  }
+    this.props.newPost(name.value, text.value);//calls a function in app.js with the name and text values
+    
+    this._name.value = ""; //clears the input tag
+    this._text.value = "";
+  };
 
   render() {
-    const posts = this.getPosts();
+    // const posts = this.getPosts();
     return (
-      <form onSubmit={this.formSubmission.bind(this)}>
-        <fieldset>
-          <input type="text" placeholder="Name" required ref={(input) => this._name = input}/>
-          
-          <input type="text" placeholder="What's New" required ref={(input) => this._text = input}/>
+      <form className="postForm" onSubmit={this.formSubmission.bind(this)}>
+        <input type="text" placeholder="Name:" required ref={(input) => this._name = input}/>
+        
+        <textarea placeholder="What's New:" required ref={(textarea) => this._text = textarea}/>
 
-          <button type="submit">Submit</button>
-        </fieldset>
-        <div>{posts}</div>
+        <button type="submit">Submit</button>
       </form>
     );
   }
 }
-
-export default {
-  Form,
-  form
-};
-
+export default Form;
         
